a29487b6f3e3351ab4b55ead8faf2315ae90c866
kromitj <kroskam8457@hotmail.com>
2015-06-28 21:20:16 -0500

    Initial Commit

---
+++ giftbuddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  giftbuddy
+//
+//  Created by Administrator on 6/28/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ giftbuddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  giftbuddy
+//
+//  Created by Administrator on 6/28/15.
+//  Copyright (c) 2015 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
